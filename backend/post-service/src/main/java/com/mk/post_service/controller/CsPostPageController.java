00e7e83302a74fa1d1ac1b1a7e9c6a63478f9da7
MinKyeom <minkyeom@naver.com>
2026-07-06 08:44:29 +0900

    page:CsPostPage make start

---
+++ backend/post-service/src/main/java/com/mk/post_service/controller/CsPostPageController.java
@@ -0,0 +1,15 @@
+package com.mk.post_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+
+public class CsPostPageController {
+  
+}
